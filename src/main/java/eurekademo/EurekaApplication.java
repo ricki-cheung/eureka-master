00aed449b5387c3195099be6ff70b0ae87ecc0d6
ricki <andyzzr@sina.com>
2018-05-24 19:26:42 +0800

    修改配置

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -2,6 +2,7 @@ package eurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
