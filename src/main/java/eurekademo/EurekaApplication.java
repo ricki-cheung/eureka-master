046bf275b42f84dc08e0472dbce8efcd9bd27aaa
ricki <andyzzr@sina.com>
2018-05-24 21:37:03 +0800

    second commit

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -1,9 +1,12 @@
 package eurekademo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -13,7 +16,16 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 @EnableConfigServer
+@RestController
 public class EurekaApplication {
+	
+	@Value("${spring.application.name}")
+	String content;
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "content is "+content;
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EurekaApplication.class, args);
