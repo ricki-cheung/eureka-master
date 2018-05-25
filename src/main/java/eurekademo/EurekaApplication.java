234e688a0335a965d5f170a64e046c81980cd949
ricki <andyzzr@sina.com>
2018-05-25 19:27:41 +0800

    提交配置

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -3,7 +3,6 @@ package eurekademo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableConfigServer
 @RestController
 public class EurekaApplication {
 	
@@ -29,6 +27,7 @@ public class EurekaApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EurekaApplication.class, args);
+		//new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
 	}
 
 }
